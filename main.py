070e9456075d12829879b0950854e24cdc20ffba
skkuyuwall <yuwall05@g.skku.edu>
2024-04-11 18:53:28 +0900

    SWE_2021_41_week_6

---
+++ main.py
@@ -0,0 +1,13 @@
+from typing import List
+
+def twoSum(nums: List[int], target: int) -> List[int]:
+    #YOUR ANSWER
+    AnswerList = []
+    
+    for i in range(0, len(nums)-1):
+        for j in range(i+1, len(nums)):
+            if nums[i]+nums[j]==target:
+                AnswerList.append(i)
+                AnswerList.append(j)
+                
+    return AnswerList;
